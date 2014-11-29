5fd19a87c4bbd82a60f92dc874db0ffb8eeb577a
Michael Nygard <mtnygard@gmail.com>
2014-11-29 11:50:57 -0600

    Add CI script

--- project.clj
+++ project.clj
@@ -7,7 +7,13 @@
   :dependencies [[org.clojure/clojure            "1.7.0-alpha4"]
                  [com.datomic/datomic-pro        "0.9.4815.12"]]
 
-  :profiles     {:dev {:dependencies [[org.clojure/test.check      "0.5.8"]]}}
+  :profiles     {:dev {:dependencies [[org.clojure/test.check      "0.5.8"]]
+                       :aliases {"ci" ["do" "clean," "test," "doc," "jar"]}}}
+
+  ;; Add the follownig to your ~/.lein/profiles.clj
+  ;; {:user {:repositories {"my.datomic.com" {:username "xxx" :pasword "xxx"}}}}
+  ;; where the username and password come from your account at my.datomic.com
+  :repositories {"my.datomic.com" {}}
 
   :jvm-opts     ["-Xmx2g" "-Xms2g" "-server" "-Ddatomic.objectCacheMax=128m"
                  "-Ddatomic.memoryIndexMax=256m" "-Ddatomic.memoryIndexThreshold=32m"])
