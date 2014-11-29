184bf350b1f8d422c5343876fd1477ab5dea23a0
Michael Nygard <mtnygard@gmail.com>
2014-11-29 12:08:02 -0600

    Shift dependency to datomic-free. Bump to latest version.

--- project.clj
+++ project.clj
@@ -5,15 +5,7 @@
   :plugins      [[codox "0.8.10"]]
 
   :dependencies [[org.clojure/clojure            "1.7.0-alpha4"]
-                 [com.datomic/datomic-pro        "0.9.4815.12"]]
+                 [com.datomic/datomic-free       "0.9.5078"]]
 
   :profiles     {:dev {:dependencies [[org.clojure/test.check      "0.5.8"]]
-                       :aliases {"ci" ["do" "clean," "test," "doc," "jar"]}}}
-
-  ;; Add the follownig to your ~/.lein/profiles.clj
-  ;; {:user {:repositories {"my.datomic.com" {:username "xxx" :pasword "xxx"}}}}
-  ;; where the username and password come from your account at my.datomic.com
-  :repositories {"my.datomic.com" {}}
-
-  :jvm-opts     ["-Xmx2g" "-Xms2g" "-server" "-Ddatomic.objectCacheMax=128m"
-                 "-Ddatomic.memoryIndexMax=256m" "-Ddatomic.memoryIndexThreshold=32m"])
+                       :aliases {"ci" ["do" "clean," "test," "doc," "jar"]}}})
