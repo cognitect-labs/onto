fa24292c5e0bc5a319fdd965705ad86e3f0689ae
Michael Nygard <mtnygard@gmail.com>
2016-09-06 15:39:33 -0500

    Update license, dep versions

--- project.clj
+++ project.clj
@@ -1,15 +1,16 @@
 (defproject com.cognitect/onto "0.1.0-SNAPSHOT"
   :description  "Ontological inferencing for Datomic"
-  :license      {:name "Copyright 2014, Cognitect, Inc. All rights reserved."}
+  :license      {:name "Eclipse Public License"
+                 :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :plugins      [[codox "0.8.10"]]
 
-  :dependencies [[org.clojure/clojure            "1.7.0-alpha4"]
-                 [com.datomic/datomic-free       "0.9.5078"]]
+  :dependencies [[org.clojure/clojure            "1.8.0"]
+                 [com.datomic/datomic-free       "0.9.5394"]]
 
-  :codox        {:src-dir-uri "https://github.com/cognitect/onto/blob/master/"
+  :codox        {:src-dir-uri "https://github.com/cognitect-labs/onto/blob/master/"
                  :src-linenum-anchor-prefix "L"
                  :defaults {:doc/format :markdown}}
 
-  :profiles     {:dev {:dependencies [[org.clojure/test.check      "0.5.8"]]
+  :profiles     {:dev {:dependencies [[org.clojure/test.check      "0.9.0"]]
                        :aliases {"ci" ["do" "clean," "test," "doc," "jar"]}}})
